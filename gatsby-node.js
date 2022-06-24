97fa3984824f71b40ae6df3752062e39e4d5c3d9
Selçuk Çalışkan <selcuk.caliskan@sigun.com.tr>
2022-06-24 14:30:10 +0300

    Create pages programmatically with Gatsby

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,43 @@
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
-  })
-}
+// exports.createPages = async function ({ actions, graphql }) {
+//   const { data } = await graphql(`
+//     query {
+//       allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+//     edges {
+//       node {
+//         frontmatter {
+//           slug
+//         }
+//       }
+//     }
+//   }
+//     }
+//   `)
+
+//   //Create paginated pages for posts
+//   const postPerPage = 3
+//   const numPages = Math.ceil(data.allMdx.edges.lenght / postPerPage)
+
+//   Array.from({ length: numPages }).forEach((_, i) => {
+//     actions.createPages({
+//       path: i === 0 ? `/` : `${i + 1}`,
+//       component: require.resolve("./scr/templates/allPosts.js"),
+//       context: {
+//         limit: postPerPage,
+//         skip: i * postPerPage,
+//         numPages,
+//         currentPage: i + 1,
+//       }
+//     })
+//   })
+
+//   //Create single blog posts
+//   data.allMdx.edges.forEach(edge => {
+//     const slug = edge.node.frontmatter.slug
+//     const id = edge.node.id
+//     actions.createPage({
+//       path: slug,
+//       component: require.resolve(`./src/templates/singlePost.js`),
+//       context: { id },
+//     })
+//   })
+// }
